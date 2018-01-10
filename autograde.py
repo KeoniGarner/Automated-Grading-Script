dafad1c2f21f57e3fa11d2b78886545fee5569c9
Keoni Garner <KeoniGarner@users.noreply.github.com>
2018-01-09 20:38:41 -0800

    Added compilation, building, and running processes for the .cpp files and created example folder with single "Hello World" program.

--- autograde.py
+++ autograde.py
@@ -10,13 +10,14 @@ import argparse as arguments
 
 # setup arguments
 parser = arguments.ArgumentParser(description='Automated Grading Script.')
-parser.add_argument("-dir", dest="fileDirectory", default="./",
+parser.add_argument("-dir", dest="fileDirectory", default="./Example Files",
     help="Directory that holds all of the students' files.")
 parser.add_argument("-grades", dest="gradeFile", default="gradeFile.txt",
     help="File where grades will be recorded (.txt)")
 parser.add_argument("-tests", dest="testFile",
     help="File that contains tests to run.")
-parser.add_argument("-comp", dest="compiler",
+parser.add_argument("-comp", dest="compiler", 
+    default="C:\\Users\\Keoni Garner\\Downloads\\MinGW\\bin\\g++.exe",
     help="Path to specific compiler ")
 args = parser.parse_args()
 
@@ -24,6 +25,7 @@ args = parser.parse_args()
 studentDirectory = args.fileDirectory
 gFile = open(args.gradeFile, mode="w")
 tFile = args.testFile
+cppCompiler = args.compiler
 
 # change working directory to directory that is specified in the arguments
 os.chdir(studentDirectory)
@@ -32,11 +34,23 @@ os.chdir(studentDirectory)
 sourceFiles = glob.glob("*.cpp")
 
 for file in sourceFiles:
-    # compile, build, and run file
-    # if file output == example output
-        # write file + ":100%" + newline? to gFile
-    # else
-        # write file + ":0%" + newline? to gFile
+    # compile and build file as "a.exe"
+    compFile = subprocess.Popen([cppCompiler, file], 
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # run "a.exe" to get output and write to file.
+    compFile = subprocess.Popen(["./a.exe"], 
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, 
+        universal_newlines = True) # needed to receive strings
+
+    output = open((file + "-output.txt"), "w")
+    output.write(compFile.stdout.read())
+
+    compFile = ""
+    os.remove("a.exe")
+    output.close
+    
+
 
 # close grade file
 gFile.close()
